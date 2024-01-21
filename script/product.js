57bb9260e1e93bf62f7a5d19dcffaca805a5cf34
Abdelrhman-2002 <118940260+Abdelrhman-2002@users.noreply.github.com>
2024-01-21 15:41:00 +0200

    edits

---
+++ script/product.js
@@ -0,0 +1,80 @@
+let nav=document.querySelector('nav'); 
+window.addEventListener("scroll",function(){
+    if (window.scrollY > 50) {
+        nav.style.position ="fixed";
+        nav.style.width="100%";
+        nav.style.marginTop="0";
+        nav.style.borderRadius="0";
+        nav.style.backgroundColor="var(--nv--color)";
+    } else {
+        nav.style.position ="sticky";
+        nav.style.width="80%";
+        nav.style.marginTop="50px";
+        nav.style.borderRadius="20px";
+        nav.style.backgroundColor="var(--text--color)";
+    }
+});
+let btn = document.getElementById("btn");
+window.addEventListener("scroll",function(){
+    if (window.scrollY > 900) {
+        btn.classList.remove("hide");
+        btn.classList.add("show");
+        btn.addEventListener("click",function(){
+            window.scrollTo({
+                top: 0,
+                behavior:"smooth"
+            });
+        })
+    } else {
+        btn.classList.remove("show");
+        btn.classList.add("hide");
+    }
+    });
+    let productsContainer = document.getElementsByClassName("products")[0];
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://dummyjson.com/products");
+    xhr.send();
+    let prod = JSON.parse(window.localStorage.getItem("product-title"));
+    
+    xhr.addEventListener("readystatechange", function () {
+        if (xhr.readyState === 4) {
+            let products = (JSON.parse(xhr.response)).products;
+    
+            let str = ``;
+            for (let i = 0; i < products.length; i++) {
+                if (products[i].title === prod) {
+                    str = `
+                        <div class="products">
+                            <div class="product">
+                                <div class="slider">
+                                    <img src="${products[i].thumbnail}" id="mainSliderImage">
+                                    <div class="slides">
+                                        <img src="${products[i].images[0]}" onclick="changeSliderImage('${products[i].images[0]}')">
+                                        <img src="${products[i].images[1]}" onclick="changeSliderImage('${products[i].images[1]}')">
+                                        <img src="${products[i].images[2]}" onclick="changeSliderImage('${products[i].images[2]}')">
+                                        <img src="${products[i].images[3]}" onclick="changeSliderImage('${products[i].images[3]}')">
+                                    </div>
+                                </div>
+                                <div class="productData">
+                                    <h2 class="product-title">${products[i].title}</h2>
+                                    <p>${products[i].description}</p>
+                                    <p>brand:${products[i].brand}</p>
+                                    <p><b>$${products[i].price}</b></p>
+                                    <p><del>$${parseInt((products[i].price * (products[i].discountPercentage / 100)) + products[i].price)}</del></p>
+                                    <p>rating: ${products[i].rating}</p>
+                                    <button id="addtocart">Add To Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                }
+            }
+    
+            productsContainer.innerHTML = str;
+        }
+    });
+    
+    function changeSliderImage(newSrc) {
+        document.getElementById("mainSliderImage").src = newSrc;
+    }
+    
--- script/products.js
+++ script/products.js
@@ -1,37 +1,72 @@
-let addTaskBox = document.querySelector(".box");
-let currentUser=JSON.parse(window.localStorage.getItem("currentUser"));
-let Users=JSON.parse(window.localStorage.getItem("Users"));
-let profileImage = document.getElementById("profileImage");
-let container=document.getElementById("container");
-
-
+let nav=document.querySelector('nav'); 
+window.addEventListener("scroll",function(){
+    if (window.scrollY > 50) {
+        nav.style.position ="fixed";
+        nav.style.width="100%";
+        nav.style.marginTop="0";
+        nav.style.borderRadius="0";
+        nav.style.backgroundColor="var(--nv--color)";
+    } else {
+        nav.style.position ="sticky";
+        nav.style.width="80%";
+        nav.style.marginTop="50px";
+        nav.style.borderRadius="20px";
+        nav.style.backgroundColor="var(--text--color)";
+    }
+});
+let btn = document.getElementById("btn");
+window.addEventListener("scroll",function(){
+    if (window.scrollY > 900) {
+        btn.classList.remove("hide");
+        btn.classList.add("show");
+        btn.addEventListener("click",function(){
+            window.scrollTo({
+                top: 0,
+                behavior:"smooth"
+            });
+        })
+    } else {
+        btn.classList.remove("show");
+        btn.classList.add("hide");
+    }
+    });
 //................................................................................................................................
+
 let productsContainer = document.getElementsByClassName("products")[0];
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "https://dummyjson.com/products");
 xhr.send();
 
 xhr.addEventListener("readystatechange", function () {
-    if (xhr.readyState === 4 ) {
+    if (xhr.readyState === 4) {
         let products = JSON.parse(xhr.response);
 
-        // console.log(products.products[0]);
         let str = ``;
         for (let i = 0; i < products.products.length; i++) {
             str += `
                 <div class="product">
                     <img src="${products.products[i].thumbnail}">
-                    <p>${products.products[i].title}</p>
+                    <p class="product-title">${products.products[i].title}</p>
                     <p><b>$${products.products[i].price}</b></p>
-                    <p><del>$${parseInt((products.products[i].price*(products.products[i].discountPercentage/100))+products.products[i].price )}</del></p>
+                    <p><del>$${parseInt((products.products[i].price * (products.products[i].discountPercentage / 100)) + products.products[i].price)}</del></p>
                     <p>rating: ${products.products[i].rating}</p>
                 </div>
             `;
         }
 
         productsContainer.innerHTML = str;
+
+        let productElements = document.getElementsByClassName("product");
+        for (let i = 0; i < productElements.length; i++) {
+            productElements[i].addEventListener("click", function (event) {
+                let clickedProductTitle = event.currentTarget.querySelector(".product-title").innerText;
+                window.localStorage.setItem("product-title",JSON.stringify(clickedProductTitle));
+                window.location.href="../html/product.html"
+            });
+        }
     }
 });
+
 
 
 
